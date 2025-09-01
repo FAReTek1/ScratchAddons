ce6efc81d0b6e350c1d64f5c6276e89e1822228b
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-09-01 20:33:14 +0100

    feat: use faretek api instead of pyodide faretek api is open source and hosted on vercel: https://github.com/FAReTek1/faretek-api/

--- addons/goboscript/userscript.js
+++ addons/goboscript/userscript.js
@@ -1,6 +1,4 @@
-/* global chrome */
-import * as sb2gs from "./sb2gs.js";
-import * as pyodidePkg from "./../../libraries/thirdparty/pyodide/pyodide.mjs";
+import downloadBlob from "../../libraries/common/cs/download-blob.js";
 
 /**
  * @param addon {UserscriptAddon}
@@ -13,32 +11,26 @@ export default async function ({ addon, console }) {
   decompileButton.title = "Decompile to goboscript code & download.";
   decompileButton.appendChild(document.createElement("span")).innerText = "Decompile";
 
-  const pyodidePromise = pyodidePkg.loadPyodide();
-
-  const pyodidePackagingPromise = pyodidePromise.then(async (pyodide) => {
-    // console.log("PYODIDE LOADED");
-
-    const loadMicropipPromise = pyodide.loadPackage("micropip");
-
-    const sb2gsWhlFile = await (await fetch(`${addon.self.dir}/${sb2gs.sb2gsWhlName}`)).bytes();
-
-    pyodide.FS.writeFile(`/${sb2gs.sb2gsWhlName}`, sb2gsWhlFile);
-
-    await loadMicropipPromise;
-
-    // console.log("EXITING PYODIDE PACKAGING");
-    return pyodide;
-  });
-
   decompileButton.onclick = async (e) => {
-    // console.log("DECOMPILE BUTTON CLICKED");
-    const pyodide = await pyodidePackagingPromise;
-    // console.log("AWAITED PYODIDE PACKAGING");
-
     decompileButton.classList.remove("waiting");
     decompileButton.classList.add("loading");
+
+    const username = await addon.auth.fetchUsername();
+    const projectAuthor = addon.tab.redux.state.preview.projectInfo.author?.username;
+
+    const isOwn = username === projectAuthor;
+
+    const title = isOwn ? document.querySelector(".project-title input") : document.querySelector(".project-title");
+    const titleStr = isOwn ? title.value : title.innerText;
+
+    const projectId = window.location.pathname.split("/")[2];
+
     try {
-      await sb2gs.decompile(addon, console, pyodide, e.shiftKey !== addon.settings.get("beginFilenameWithId"));
+      const beginFilenameWithId = e.shiftKey !== addon.settings.get("beginFilenameWithId");
+
+      const data = await (await fetch(`https://faretek-api.vercel.app/api/sb2gs/?id=${projectId}`)).blob();
+      downloadBlob((beginFilenameWithId ? `${projectId} ` : "") + titleStr + ".zip", data);
+
     } catch (err) {
       window.alert(err);
     }
